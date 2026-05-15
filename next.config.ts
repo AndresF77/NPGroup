93020ea402adb920e8a30d6a4b662a26664cde96
Andres Ferrara <andre.ferrara1019@gmail.com>
2026-05-15 18:18:28 +0000

    feat: update next config file

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["8fq3cv-3000.csb.app"],
 };
 
 export default nextConfig;
